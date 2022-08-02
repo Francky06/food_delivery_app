7e6bffbace37bbb0aa009dbd8d2a03e572a8ff4f
Francky06 <ffroswell@gmx.fr>
2022-08-02 20:06:55 +0200

    effet scale header

--- pizza_app/lib/home/food_page_body.dart
+++ pizza_app/lib/home/food_page_body.dart
@@ -2,19 +2,44 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pizza_app/utils/colors.dart';
 import 'package:pizza_app/widgets/big_text.dart';
+import 'package:pizza_app/widgets/icon_and_text.dart';
 import 'package:pizza_app/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
+  const FoodPageBody({Key? key}) : super(key: key);
   @override
   _FoodPageBodyState createState() => _FoodPageBodyState();
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
+  var _currPageValue = 0.0;
+  double _scaleFactor = 0.8;
+  double _height = 220;
+
+  //Index du carousel
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue =  pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+  }
+
+
+// carousel
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
       height: 320,
       child: PageView.builder(
         controller: pageController,
@@ -24,60 +49,117 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       }),
     );
   }
+
+
+  // scale header effect, master flutter ;)
+
   Widget _buildPageItem(int index){
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: EdgeInsets.only(left: 10, right: 10),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: index.isEven? Color(0xFF69c5df) : Color(0xFF9294cc),
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage(
-                      "assets/img/pizza-blanche.jpg"
-                  )
-              )
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 120,
-            margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+    Matrix4 matrix = new Matrix4.identity();
+    if(index == _currPageValue.floor()) {
+      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTransf = _height*(1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTransf, 0);
+
+    } else if ( index == _currPageValue.floor()+1) {
+      var currScale = _scaleFactor+(_currPageValue-index+1)*(1-_scaleFactor);
+      var currTransf = _height*(1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTransf, 0);
+
+    } else if ( index == _currPageValue.floor()-1) {
+      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTransf = _height*(1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTransf, 0);
+    } else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height*(1-_scaleFactor)/2, 0);
+    }
+
+
+    //header
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: EdgeInsets.only(left: 10, right: 10),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
-                color: Colors.white
-
-            ),
-            child: Container(
-              padding: EdgeInsets.only(top: 10, left: 15, right: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BigText(text: "Pizza du moment"),
-                  SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Wrap(
-                        children: List.generate(5, (index) => Icon(Icons.star, color: AppColors.mainColor, size: 15))
-                      ),
-                      SizedBox(width: 10),
-                      SmallText(text: "4.9"),
-                      SizedBox(width: 10),
-                      SmallText(text: "123"),
-                      SizedBox(width: 5),
-                      SmallText(text: "Commentaires"),
-
-                    ],
-                  ),
-                ],
-              ),
+                color: index.isEven? Color(0xFF69c5df) : Color(0xFF9294cc),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                        "assets/img/pizza-blanche.jpg"
+                    )
+                )
             ),
           ),
-        )
-      ],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xFFe8e8e8),
+                      blurRadius: 5.0,
+                      offset: Offset(0,5)
+                    ),
+                    BoxShadow(
+                        color: Colors.white,
+                        offset: Offset(-5,0)
+                    ),
+                    BoxShadow(
+                        color: Colors.white,
+                        offset: Offset(5,0)
+                    )
+                  ]
+
+
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Pizza du moment"),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) => Icon(Icons.star, color: AppColors.mainColor, size: 15))
+                        ),
+                        SizedBox(width: 10),
+                        SmallText(text: "4.9"),
+                        SizedBox(width: 10),
+                        SmallText(text: "123"),
+                        SizedBox(width: 5),
+                        SmallText(text: "Commentaires"),
+
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconAndText(icon: Icons.circle_sharp, text: "Normal", iconColor: AppColors.iconColor1),
+                        IconAndText(icon: Icons.location_on, text: "1.7Km", iconColor: AppColors.mainColor),
+                        IconAndText(icon: Icons.access_time_filled_rounded, text: "27 min", iconColor: AppColors.iconColor2),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
